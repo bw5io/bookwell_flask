0fe60c0967cd0c065eb7e754402ecb3bdc1ecf0e
Bill Wu <hi@bw5.io>
2021-12-27 20:29:28 +0000

    Database Update: Removed mobile phone field, renamed user_type

--- bookwell_system/models.py
+++ bookwell_system/models.py
@@ -10,7 +10,7 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(60), nullable=False)
     first_name = db.Column(db.String(80))
     last_name = db.Column(db.String(80))
-    mobile_phone = db.Column(db.String(11))
+    user_type = db.Column(db.Integer, nullable=False, default=0)
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
 
