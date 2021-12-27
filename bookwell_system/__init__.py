b4266b3040ef845235abd80172f47d7b20c2d93a
Bill Wu <hi@bw5.io>
2021-12-27 20:29:55 +0000

    Blueprint registered in entry point

--- bookwell_system/__init__.py
+++ bookwell_system/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_admin import Admin
+from flask_migrate import Migrate
 import os
 
 app = Flask(__name__)   
@@ -9,10 +9,18 @@ app.config['SECRET_KEY']=os.environ.get("SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URI")
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+from .main import main as main_blueprint
+app.register_blueprint(main_blueprint)
 
-from . import routes
+from .admin import admin as admin_blueprint
+app.register_blueprint(admin_blueprint, url_prefix="/admin")
+
+from .auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint, url_prefix="/auth")
+
 from .models import *
