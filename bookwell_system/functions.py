92488ff9be909f1c84d03aa3acc6667aa5980d2a
Bill Wu <hi@bw5.io>
2021-12-31 16:07:11 +0000

    Adding Compare validator

--- bookwell_system/functions.py
+++ bookwell_system/functions.py
@@ -1,8 +1,50 @@
 from flask import flash
+from wtforms.validators import ValidationError
 
 def flash_errors(form):
-    """Flashes form errors"""
+    """
+    Flashes form errors
+    """
     for _, errors in form.errors.items():
         for error in errors:
             flash( (error), 'error')
 
+class Compare(object):  # --> Change to 'LessThan'
+    """
+    Compares the values of two fields.
+
+    :param fieldname:
+        The name of the other field to compare to.
+    :param message:
+        Error message to raise in case of a validation error. Can be
+        interpolated with `%(other_label)s` and `%(other_name)s` to provide a
+        more helpful error.
+    """
+    def __init__(self, fieldname, message=None, direction="left"):
+        self.fieldname = fieldname
+        self.message = message
+        self.direction = direction
+
+    def __call__(self, form, field):
+        try:
+            other = form[self.fieldname]
+        except KeyError:
+            raise ValidationError(field.gettext("Invalid field name '%s'.") % self.fieldname)
+        if self.direction=="left":
+            validate=field.data>other.data
+        elif self.direction=="right":
+            validate=field.data<other.data
+        elif self.direction=="equal":
+            validate=field.data==other.data
+        else:
+            raise ValidationError("Direction not defined")
+        if not validate:  #  --> Change to >= from !=
+            d = {
+                'other_label': hasattr(other, 'label') and other.label.text or self.fieldname,
+                'other_name': self.fieldname
+            }
+            message = self.message
+            if message is None:
+                message = field.gettext('Validation Failed.')
+
+            raise ValidationError(message % d)
